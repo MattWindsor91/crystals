62e10a23fa1a0db6bb31c07f5cdec856e48ed0f8
Alexander Preisinger <alexander.preisinger@gmail.com>
2010-09-22 12:38:12 +0200

    added test module for python bindings

---
+++ modules/bindings-python.c
@@ -0,0 +1,45 @@
+
+#include <Python.h>
+
+/** Initialise the python module. */
+
+int
+init (void);
+
+/** De-initialise the python module. */
+
+void
+term (void);
+
+/** Test the python module. */
+
+void
+test (void);
+
+void
+init (void)
+{
+    Py_Initialize();
+
+    /* set the patch in which the python files are */
+    PySys_SetPath(".");
+}
+
+void
+term (void)
+{
+    PyFinalize();
+}
+
+void
+test (void)
+{
+    PySys_SetPath("./test");
+
+    char* test =    "import sys"
+                    "print sys.argv"
+                    "print sys.path";
+
+    /* a simple test */
+    PyRun_SimpleString(test);
+}
