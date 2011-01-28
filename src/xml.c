8da026f050bdf5f6c6108f22734d32b4c72ec9b2
Alexander Preisinger <alexander.preisinger@gmail.com>
2011-01-29 00:00:01 +0100

    Initial xml module

---
+++ src/xml.c
@@ -0,0 +1,86 @@
+/*
+ * Crystals (working title)
+ *
+ * Copyright (c) 2010 Matt Windsor, Michael Walker and Alexander
+ *                    Preisinger.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ *   * The names of contributors may not be used to endorse or promote
+ *     products derived from this software without specific prior
+ *     written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AFOREMENTIONED COPYRIGHT HOLDERS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** 
+ * @file    src/xml.c
+ * @author  Alexander Preisinger
+ * @brief   XML module.
+ * @todo    Test!
+ */
+ 
+#include "xml.h"
+
+xmlNode 
+parse_xml_file (const char *p)
+{    
+  xmlDoc *doc = NULL;
+  xmlNode *root = NULL;
+
+  /*
+   * this initialize the library and check potential ABI mismatches
+   * between the version it was compiled for and the actual shared
+   * library used.
+   */
+  LIBXML_TEST_VERSION
+
+  doc = xmlReadFile (p, NULL, 0);
+
+  if (doc == NULL) {
+      /* fprintf instead of error for testing purposes */
+      fprintf (stderr, "XML - parse_xml_file - could not parse file %s", p);
+  }
+  else
+    /*Get the root element node */
+    root = xmlDocGetRootElement(doc);
+    
+  return root;
+}
+
+void
+free_xml_file (xmlNode *root)
+{
+  xmlFreeNode (root); /**<< @todo check if it frees all connected nodes too, 
+                                  or if it just frees one node */
+  root = NULL;
+}
+
+void
+cleanup_xml (void) 
+{
+  xmlCleanupParser ();
+}
