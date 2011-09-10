9dfa0dd3d185db7ec5206b60e76f410699b58f45
Michael Walker <mike@barrucadu.co.uk>
2011-09-10 16:54:43 +0100

    Added missing module header

---
+++ src/modules/module.h
@@ -0,0 +1,62 @@
+/*
+ * Crystals (working title)
+ *
+ * Copyright (c) 2010 Matt Windsor, Michael Walker and Alexander
+ * Preisinger.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ *
+ * * The names of contributors may not be used to endorse or promote
+ * products derived from this software without specific prior
+ * written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AFOREMENTIONED COPYRIGHT HOLDERS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file   src/modules/module.h
+ * @author Michael Walker
+ * @brief  Header to include all other required headers for modules.
+ */
+
+#ifndef _MODULE_H
+#define _MODULE_H
+
+/* External headers */
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+
+#include <glib.h>
+#include <glib-2.0/glib/gslist.h>
+
+/* Crystals headers */
+#include "../types.h"
+#include "../util.h"
+#include "../events.h"
+#include "../graphics.h"
+
+#endif /* not _MODULE_H */
