864520e1f6766dfc865774a66fc0e22ade2c8511
Alexander Preisinger <alexander.preisinger@gmail.com>
2012-01-22 15:32:21 +0100

    src/tests: Add dialog tests

---
+++ src/tests/dialog/dialog-test.c
@@ -0,0 +1,88 @@
+/*
+ * Crystals (working title)
+ *
+ * Copyright (c) 2010 Matt Windsor, Michael Walker and Alexander
+ *                    Preisinger.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ *   * The names of contributors may not be used to endorse or promote
+ *     products derived from this software without specific prior
+ *     written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AFOREMENTIONED COPYRIGHT HOLDERS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** 
+ * @file    src/tests/dialog-test.c
+ * @author  Alexander Preisinger
+ * @brief   Simple dialog tests
+ */
+
+#include <stdio.h>
+
+#define BUILD_TEST 1
+
+#include "dialog.h"
+
+
+#if 0
+dlg_t*
+dlg_parse_file (const char *p);
+
+dlg_content_t*
+dlg_content_next (dlg_t *dlg, uint8_t choice);
+
+req_t*
+dlg_requirement_next (dlg_t *dlg);
+
+void
+dlg_free (dlg_t *dlg);
+#endif
+
+int main(int argc, const char *argv[])
+{
+    printf("test\n");
+    dlg_t * test_dlg = dlg_parse_file ("./test-dialog.dlg.xml");
+
+    dlg_content_t * con = NULL;
+
+    printf("test\n");
+
+    while ((con = dlg_content_next (test_dlg, 0)) != 0) 
+      {
+        if (con->type == TEXT)
+          {
+            printf("%20s: %s\n", con->action.text.actor_id, con->action.text.text);
+          }
+      }
+
+    dlg_free (test_dlg);
+    return 0;
+}
+
+/* vim: set ts=2 sw=2 softtabstop=2 cinoptions=>4,n-2,{2,^-2,:2,=2,g0,h2,p5,t0,+2,(0,u0,w1,m1: */
+
